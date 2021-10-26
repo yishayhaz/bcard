96520d18f486277beddbe91a221b5e31a8e096e5
Yishay Hazan <yishay11013@gmail.com>
2021-10-26 10:10:14 +0300

    meta tags impr

--- public/js/bcard.js
+++ public/js/bcard.js
@@ -1,7 +1,8 @@
 let img = document.querySelector('img.logo');
 
 img.addEventListener('load', () => {
-    if(Math.abs(img.width - img.height) * 100 / Math.max(img.width - img.height) < 25) {
+    console.log(img.height, img.width)
+    if(Math.abs(img.width - img.height) * 100 / Math.max(img.width, img.height) < 25) {
         img.width = img.height;
         img.style.borderRadius = '50%';
     }
